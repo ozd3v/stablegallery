cefcdf1cfb84dfe9935559b91658ec25fd153d33
ozd3v <oz.jedah@gmail.com>
2023-04-18 14:56:35 -0400

    for znabato

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,12 +1,12 @@
 export const settings = {
-    destinationFolder: '/mnt/shares/thumbnails',
-    baseFolder: '/mnt/shares/sdimages/',
+    destinationFolder: 'C:/Users/zanbato/Desktop/laragon-portable/www/sdv2/stable-diffusion-webui/thumbnails', //'/mnt/shares/thumbnails',
+    baseFolder: 'C:/Users/zanbato/Desktop/laragon-portable/www/sdv2/stable-diffusion-webui/outputs/', //'/mnt/shares/sdimages/
     img2imggrid: 'img2img-grids',
     txt2imggrid: 'txt2img-grids',
     extrasImages: 'extras-images',
-    getAllFiles: 'https://ozjedah.hopto.org/api/getAllFiles',
-    getImage: 'https://ozjedah.hopto.org/api/getImage',
-    deleteImage: 'https://ozjedah.hopto.org/api/deleteImage',
+    getAllFiles: 'http://localhost:3000/api/getAllFiles',
+    getImage: 'http://localhost:3000/api/getImage',
+    deleteImage: 'http://localhost:3000/api/deleteImage',
 
 }
 // http://localhost:3000/api/getAllFiles
